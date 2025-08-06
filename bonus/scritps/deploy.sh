79e636f9f1107454eb3c56ddc669e726696b6577
Administrator <gitlab_admin_2e3564@example.com>
2025-08-06 17:51:34 +0400

    idk

--- bonus/scritps/deploy.sh
+++ bonus/scritps/deploy.sh
@@ -1,8 +1,22 @@
-k apply -f namespace.yaml
-sudo snap install --classic
+k3d cluster create gitlab-cluster \
+  --agents 2 \
+  --port "8080:80@loadbalancer" \
+  --port "8443:443@loadbalancer"
 
+
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-definition-v1.yml
+kubectl apply -f https://raw.githubusercontent.com/traefik/traefik/v2.10/docs/content/reference/dynamic-configuration/kubernetes-crd-rbac.yml
+
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+
+helm install traefik traefik/traefik --namespace kube-system
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
-helm install gitlab gitlab/gitlab \
-  --namespace gitlab \
-  --values gitlab-values.yaml
+kubectl create namespace gitlab
+helm install gitlab gitlab/gitlab -f confs/gitlab-values.yaml --namespace gitlab
+helm repo add gitlab-runner https://charts.gitlab.io/
+helm repo update
+kubectl create namespace gitlab-runner
+
+helm install gitlab-runner gitlab-runner/gitlab-runner --namespace gitlab-runner
